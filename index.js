9ef85cd09aae94b547e022b86512fd880af3b0f4
cheboi <moscheb96@gmail.com>
2023-10-01 13:05:53 +0300

    user login

--- index.js
+++ index.js
@@ -8,9 +8,27 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+const users = [];
+const threadList = [];
+
 app.get("/api", (req, res) => {
   res.json({
     message: "Testing World",
+  });
+});
+
+// Login
+app.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+  let result = users.filter((user) => {
+    user.email === email && user.password === password;
+  });
+  if (result.length !== 1) {
+    res.status(403).send("Invalid credentials");
+  }
+  res.json({
+    message: "Login Successful",
+    id: result[0].id,
   });
 });
 
